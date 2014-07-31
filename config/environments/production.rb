e6b4fcdeaffb819472d41e877cac0f2ef6425a57
garrett <g.kocher@gmail.com>
2014-07-31 13:02:47 -0700

    add amazon s3 for image upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # Required for Paperlip
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credientials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
 end
